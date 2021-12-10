9fba4ecc1ab5958a53510531894cac792bdfffea
exembly <zachary.parish@ontariotechu.net>
2021-12-10 14:58:11 -0800

    set device

--- train_funcs.py
+++ train_funcs.py
@@ -52,7 +52,7 @@ def train_ddp_ce(rank, world_size, model,
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
-    #torch.cuda.set_device(rank)
+    torch.cuda.set_device(rank)
 
     model = model.cuda(rank)
 
